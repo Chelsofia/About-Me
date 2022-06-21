a7726b32903c7983c80d8782cdc313e80e28e1d0
Rachel Isaac <86608086+Chelsofia@users.noreply.github.com>
2022-06-21 02:39:31 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,18 @@
+const myName = (first_name, last_name) => {
+  return `My name is ${first_name} ${last_name}`;
+};
+document.write(myName("Rachel", "Asse"));
+document.write('<br>')
+document.write("<br>");
+
+const myHeight = ( height) => {
+  return `I am  ${height}`;
+};
+document.write(myHeight("5'8")); 
+document.write("<br>");
+document.write("<br>");
+
+const myCountry = (country_name) => {
+  return `I'm from  ${country_name}`;
+};
+document.write(myCountry("Nigeria")); 
